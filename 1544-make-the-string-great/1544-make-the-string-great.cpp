fde4d68ba36635815288c82e3400fc8aec740537
AliSweidan <86023430+Alisweid168@users.noreply.github.com>
2022-11-08 18:35:45 +0200

    Time: 8 ms (30.28%), Space: 6.2 MB (82.16%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -4,10 +4,12 @@ public:
         string s2;
         for(int i=0;i<s.size();i++){
             s2.push_back(s[i]);
-            while(s2.size() && ((s[i+1]==32+s2.back()) ||(s[i+1]==s2.back()-32)) ){
+           
+            while(s2.size() && ((s[i+1]==s2.back()+32)  ||(s[i+1]==s2.back()-32))){
                 s2.pop_back();
                 i++; 
             }
+           
         }
         return s2;
     }
