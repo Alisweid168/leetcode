0c2028a743685a6e31a628e049e5b42dceba5917
Alisweid168 <ali.sweidan003@gmail.com>
2024-03-23 20:47:30 +0200

    two-sum-sol

---
+++ two-sum/two-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+     int[] arr=new int[2];
+     Map<Integer,Integer> mp=new HashMap<>();
+     for(int i=0;i<nums.length;i++){
+        if(mp.containsKey(target-nums[i])){
+            arr[0]=i;
+            arr[1]=mp.get(target-nums[i]);
+            return arr;
+        }
+        mp.put(nums[i],i);
+        }
+     return arr;
+    }
+}
+
